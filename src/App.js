a521ae63f45004c7a1d2a5fa4c53f7dc97370800
Italo Marcelo <137944249+MarceloDevd3@users.noreply.github.com>
2024-06-12 15:27:34 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import useCountDown from './components/hooks/useCounDown'
 
 function App() {
 
-  const [day, hour, Minute, segundo] = useCountDown('jan 1, 2024 00:00:00') ;
+  const [day, hour, Minute, segundo] = useCountDown('jan 1, 2025 00:00:00') ;
   return (
     <main>
     <img src="https://images.unsplash.com/photo-1514876246314-d9a231ea21db?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
